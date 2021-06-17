afeca074f9fe8ebafbf68b3cd7a3bf8bfab117bf
Krutika Mohanty <bmohanty@gmail.com>
2021-06-17 13:21:12 -0400

    data modification

--- src/main/java/com/bizz/stocks/bizzfinanceops/controller/StockController.java
+++ src/main/java/com/bizz/stocks/bizzfinanceops/controller/StockController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Krutika Mohanty
+ * @author Krutika Mohanty 
  */
 @RestController
 public class StockController {
